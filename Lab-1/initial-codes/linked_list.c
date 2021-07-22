6356093cacafd4702deef4c199807faab4131f17
Sasindu Alahakoon <47134066+SasinduDilshara@users.noreply.github.com>
2021-07-22 22:16:36 +0530

    Update

--- Lab-1/initial-codes/linked_list.c
+++ Lab-1/initial-codes/linked_list.c
@@ -1,60 +1,44 @@
-struct list_node_s {
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
     int data;
-    struct list_node_s* next;
-}
+    struct node *next;
+};
 
-int Member(int value, struct list_node_s* head_p) {
-    struct list_node_s* curr_p = head_p;
-    while (curr_p != NULL && curr_p->data < value)
-        curr_p = curr_p->next;
+int Member(int value, struct node *linked_list) {
+    struct node *current = linked_list;
 
-    if (*curr_p == NULL || curr_p->data > value) {
+    while (current != NULL && current->data < value) {
+        current = current->next;
+    }
+
+    if (current == NULL || current->data >value) {
         return 0;
     } else {
         return 1;
     }
 }
 
-int Insert ( int value, struct list_node_s* head_pp) {
-    struct list_node_s* curr_p = *head_pp;
-    struct list_node_s* pred_p = NULL;
-    struct list_node_s* temp_p;
+int Insert(int value, struct node **linked_list) {
+    struct node *current_node = *linked_list;
+    struct node *pred_p = NULL;
+    struct node *temp_p;
 
-    while (curr_p !- NULL && curr_p->data < value) {
-        pred_p = curr_p;
-        curr_p = curr_p->next;
+    while (current_node != NULL && current_node->data < value) {
+        pred_p = current_node;
+        current_node = current_node->next;
     }
 
-    if (curr_p == NULL || curr_p->data > value) {
-        temp_p = malloc(sizeof(struct list_node_s));
+    if (current_node == NULL || current_node->data > value) {
+        temp_p = (struct node*) malloc(sizeof(struct node));
         temp_p->data = value;
-        temp->next = curr_p;
-        if (pred_p == NULL)
-            *head_pp = temp_p;
-        else 
-            pred_p->next = temp_p;
-        return 1;
-    } else {
-        return 0;
-    }
-}
+        temp_p->next = current_node;
 
-int Delete (int value, struct list_node_s** head_pp) {
-    struct list_node_s* curr_p = *head_pp;
-    struct list_node_s* pred_p = NULL;
-
-    while (curr_p != NULL & curr_p->data < value) {
-        pred_p = curr_p;
-        curr_p = curr_p->next;
-    }
-
-    if *(curr_p != NULL && curr_p->data == value) {
         if (pred_p == NULL) {
-            *head_pp = curr_p->next;
-            free(curr_p);
+            *linked_list = temp_p;
         } else {
-            pred_p->next = curr_p->next;
-            free(curr_p);
+            pred_p->next = temp_p;
         }
         return 1;
     } else {
@@ -62,8 +46,57 @@ int Delete (int value, struct list_node_s** head_pp) {
     }
 }
 
-int main() {
-    printf("Hello");
+int Delete(int value, struct node **linked_list) {
+    struct node *current = *linked_list;
+    struct node *pred_p = NULL;
+
+    while (current != NULL && current->data < value) {
+        pred_p = current;
+        current = current->next;
+    }
+
+    if (current != NULL && current->data == value) {
+        if (pred_p == NULL) {
+            *linked_list = current->next;
+            free(current);
+        } else {
+            pred_p->next = current->next;
+            free(current);
+        }
+        return 1;
+    } else {
+        return 0;
+    }
+
 }
 
-// gcc -g -Wall -o out linked_list.c -Ipthread
+void Print(struct node *root) {
+    struct node *current = root;
+
+    while(current->next !=NULL) {
+        printf("(%d)\n |\n\\/\n",current->data);
+        current = current->next;
+    }
+    printf("\n");
+}
+
+int main(int argc, char *argv[]) {
+
+    printf("Sequential Linked List Testing\n");
+    
+    int iter = atoi(argv[1]);
+    long max = strtol(argv[2], NULL, 10);
+
+    struct node *root;
+    int random = rand() % max;
+    root = (struct node*) malloc(sizeof(struct node));
+    root->data = random;
+
+    for (int i=0; i < iter; i++) {
+        int random = rand() % max;
+        Insert(random, &root);
+    }
+
+    Print(root);
+    
+}
